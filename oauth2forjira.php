04c9eef89a976594315cc88e2f8bbd36ad50d10b
discaR <distance_scar@163.com>
2016-07-05 14:25:34 +0800

    Add files via upload

---
+++ oauth2forjira.php
@@ -0,0 +1,32 @@
+<?php
+$ch = curl_init();
+$url = 'https://jira.atlassian.com/rest/api/2/users';
+$url2 = 'http://jira.local.com/rest/api/2/user';
+$url3 = 'http://gitlab.local.com/api/v3/users';
+$postData = array('name'=>'mytest', 'password'=>'qwe123789', 'emailAddress'=>'mytest@qq.com',
+    'displayName'=>'testz','applicationKey'=>'jira-core');
+$postData2 = '{"name":"charlie2","password":"123123","emailAddress":"charlie2@test.com","displayName":"Charlie of Atlassian","applicationKeys":["jira-core"]}';
+$postData2 = json_encode($postData2);
+$postData3 = array('username'=>'mytest', 'password'=>'qwe123789', 'emailAddress'=>'mytest@qq.com',
+    'name'=>'testz');
+curl_setopt($ch, CURLOPT_URL, $url2);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_HEADER, 'PRIVATE-TOKEN: P1oCt4aGBGpACJp8TxiL');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS,$postData2);
+$data = curl_exec($ch);
+curl_close($ch);
+var_dump($data);
+
+//$postdata = http_build_query(array('username'=>'mytest', 'password'=>'qwe123789', 'email'=>'mytest@qq.com','name'=>'testz'));
+//$options = array(
+//    'http' => array(
+//        'method' => 'POST',
+//        'header' => 'Content-type:application/x-www-form-urlencoded',
+//        'content' => $postdata,
+//        'timeout' => 15 * 60
+//    )
+//);
+//$context = stream_context_create($options);
+//$result = file_get_contents('http://gitlab.local.com/users', false, $context);
+//var_dump($result);
