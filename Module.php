c67a4183ba5815118eb7c61da8b7bd2a8ab2d5a1
chuanhangyu <chuanhangyu@xmisp.com>
2016-07-06 14:35:53 +0800

    update Module.php

--- Module.php
+++ Module.php
@@ -1,10 +1,20 @@
 <?php
 namespace discaR\oauth2forjira;
 
-class Module extends \yii\base\Module {
+class Module extends \yii\base\Module
+{
+    public $api;
+    public $user;
+    public $pwd;
+
     public function init()
     {
         parent::init();
-        $this->registerTranslations();
+    }
+
+    public function signUp($username, $password, $email, $name)
+    {
+        $oauth2forjira = new oauth2forjira\oauth2forjira($this->api, $this->user, $this->pwd);
+        return $oauth2forjira->signUp($username, $password, $email, $name);
     }
 }
